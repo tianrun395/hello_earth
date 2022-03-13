57cd8a88dab227679274bdb9d4ee41902558c02b
Tianrun <tz395@cornell.edu>
2022-03-13 18:19:32 -0400

    This is a commit

---
+++ src/test/java/com/tz/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tz.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
